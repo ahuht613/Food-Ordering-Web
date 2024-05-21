290b94c68517ac68bb697170ad252ec7869a24f5
AhuhtPC <tranthuha06012003@gmail.com>
2024-05-22 02:06:24 +0700

    com

--- Library/src/main/java/com/ecommerce/library/model/Customer.java
+++ Library/src/main/java/com/ecommerce/library/model/Customer.java
@@ -2,6 +2,7 @@ package com.ecommerce.library.model;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,7 +20,10 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "customer_id")
     private Long id;
+
+    @Size(min = 3, max = 15, message = "First name should have 3-15 characters")
     private String firstName;
+    @Size(min = 3, max = 15, message = "Last name should have 3-15 characters")
     private String lastName;
     private String username;
     private String country;
